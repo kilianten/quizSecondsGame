e23f59a3d14b565a5d4970afe0bf747d46a36152
kilian <kiliantennyson@live.ie>
2020-11-26 20:55:56 +0000

    Added json for questions and loaded into game

--- game.py
+++ game.py
@@ -1,15 +1,19 @@
 import pygame as pg
 from settings import *
 from sprites import *
+import json
 
 class Game():
     def __init__(self, main):
         self.main = main
         self.panel = Panel(main, 0, 0)
         self.colour_scheme = PALETTE_1
+        self.questions = json.load(open('questions/questions.json'))
+        print("Number of Questions: ",len(self.questions))
 
     def getQuesiton(self):
         self.panel = Panel(self, 0, 0)
+        print(len(self.questions))
 
     def draw(self):
         pg.draw.rect(self.main.screen, PALETTE_1[0], (0, 0, self.panel.image.get_width(), self.panel.image.get_height()))
